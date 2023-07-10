714a2b55f190af8f591be1b14e38a2d5f9cb09fc
SoeWHtun <soewana35@gmail.com>
2023-07-10 16:11:18 +0700

    Chapter4

---
+++ Chapter4/CharType.java
@@ -0,0 +1,10 @@
+public class CharType {
+    public static void main(String[] args) {
+        char letter = 'A';
+        char numletter = '4';
+        System.out.println(++letter);
+        char numchar = '\u0043';
+        System.out.println(numchar);
+        System.out.println("\"Java is fun\"");
+    }
+}
---
+++ Chapter4/TrigonometricFunctions.java
@@ -0,0 +1,11 @@
+public class TrigonometricFunctions {
+    public static void main(String[] args) {
+        System.out.println("The value of sin(Pi/2)"+ Math.sin(Math.PI/2));
+        System.out.println("The value of exp(3.5): "+Math.exp(3.5));
+        System.out.println("The value of power(2,3): "+Math.pow(2,3));
+        System.out.println(Math.ceil(2.3));
+        System.out.println(Math.floor(2.3));
+        System.out.println(Math.rint(2.5));
+    }
+    
+}
